9a2a9b48a96cdb0ae57ff5c62234d4daf82ddc45
JKolios <jason.kolios@gmail.com>
2020-08-08 00:37:51 +0300

    Adds a functional reddit trainer

--- main.py
+++ main.py
@@ -12,30 +12,62 @@ from sentence_cleaner import clean_sentence_text
 
 q = queue.Queue()
 
-def chatbot_worker(chatbot=None):
-  while True:
-      sentence = q.get()
-      sentence.text = clean_sentence_text(sentence.text)
-      response = chatbot.get_response(sentence)
-      response.text = clean_sentence_text(response.text)
-      print(f'{chatbot.name}: {response}')
-      q.put(response)
-      q.task_done()
-      sleep(0.1)
+# def chatbot_converse(chatbot=None):
+#   while True:
+#       sentence = q.get()
+#       sentence.text = clean_sentence_text(sentence.text)
+#       response = chatbot.get_response(sentence)
+#       response.text = clean_sentence_text(response.text)
+#       print(f'{chatbot.name}: {response}')
+#       q.put(response)
+#       q.task_done()
+#       sleep(0.1)
 
+# def chatbot_train(chatbot=None):
+#   bot_trainer = RedditTrainer(chatbot)
+#   while True:
+#       bot_trainer.train(10)
+#       sleep(30)
+
+def train_bot(bot):
+	bot_trainer = RedditTrainer(bot)
+	try:
+		bot_trainer.train(5)
+	except:
+		pass # Ignore any training errors for now
 
 def main():
-  q.put(Statement("Hi!"))
+	# q.put(Statement("Hi!"))
+	bots = bot_creator.create_bots(['Alice', 'Bob'])
+	conversation_index = 0
+	training_iterations = 0
+	topic = 'Hi'
+	while True:
+		if conversation_index == 100:
+			print(f'Training bots, iteration {training_iterations}')
+			for bot in bots:
+				train_bot(bot)
+			training_iterations +=1
+			conversation_index = 0
+		else:
+			for bot in bots:
+				topic = bot.get_response(topic)
+				print(f'{bot.name}: {topic}')
+				conversation_index += 1
+				# sleep(0.1)
+		
 
-  bots = bot_creator.create_bots(['Alice', 'Bob'])
-  for bot in bots:
-    bot_trainer = RedditTrainer(bot)
-    bot_trainer.train(10)
-  
-  for bot in bots:
-    threading.Thread(target=chatbot_worker, daemon=True, kwargs={'chatbot': bot}).start()
 
-  q.join()
+	
+
+
+	
+
+	# for bot in bots:
+	# threading.Thread(target=chatbot_converse, daemon=True, kwargs={'chatbot': bot}).start()
+	# threading.Thread(target=chatbot_train, daemon=True, kwargs={'chatbot': bot}).start()
+
+	# q.join()
 
 if __name__ == '__main__':
-  main()
+	main()
--- reddit_trainer.py
+++ reddit_trainer.py
@@ -8,45 +8,77 @@ class RedditTrainer(ListTrainer):
   def __init__(self, chatbot):
       super().__init__(chatbot)
       self.reddit_api = self.init_reddit_api()
-      self.subreddit = self.get_subreddit('all')
-      self.submissions = self.get_next_hot_submissions()
+      self.subreddit = None
+      self.current_submission = None
+      self.current_submission_comments_retrieved = False
+      self.current_comment = None
   
   def train(self, training_rounds):
-    for round in range(training_rounds):
-      print('Training bot: {0} round {1}'.format(self.chatbot.name, round+1))
+    for training_round in range(training_rounds):
+      print('Training bot: {0} round {1}'.format(self.chatbot.name, training_round))
+      self.subreddit = self.get_random_subreddit()
+      self.current_submission = self.get_random_submission()
+      self.current_submission_comments_retrieved = False
+
       conversation = self.get_reddit_conversation()
-      print('Conversation: {0}'.format(conversation))
-      super().train(conversation)
+      if conversation:
+        print('Conversation: {0}'.format(conversation))
+        super().train(conversation)
+      else:
+        print('No usable conversation')
 
   def get_subreddit(self, name):
+    print(f'Getting subreddit {name}')
     return self.reddit_api.subreddit(name)
 
-  def get_next_hot_submissions(self, limit=5):
+  def get_random_subreddit(self, nsfw=False):
+    print('Getting random subreddit')
+    subreddit = self.reddit_api.random_subreddit(nsfw) 
+    print(f'Got subreddit: {subreddit.display_name}')
+    return subreddit
+
+  def get_submissions(self, limit):
+    print(f'Getting {limit} submissions')
     return self.subreddit.hot(limit=limit)
 
-  
+  def get_random_submission(self):
+    print('Getting random submission')
+    submission = self.subreddit.random() 
+    print(f'Got submission: {submission.title}')
+    return submission
+
   @staticmethod
   def init_reddit_api():
-    return praw.Reddit(client_id=os.getenv('CLIENT_ID'),
-                        client_secret=os.getenv('CLIENT_SECRET'),
-                        username=os.getenv('USERNAME'),
-                        password=os.getenv('PASSWORD'),
-                        user_agent="RandomTextSource by /u/RandomWordSource",
-                        )
+    return praw.Reddit(
+      client_id=os.getenv('CLIENT_ID'),
+      client_secret=os.getenv('CLIENT_SECRET'),
+      username=os.getenv('USERNAME'),
+      password=os.getenv('PASSWORD'),
+      user_agent="RandomTextSource by /u/RandomWordSource",
+    )
 
+  def retrieve_submission_comments(self, max_replace_more_count=1):
+    print(f'Replacing up to {max_replace_more_count} replace_more for submission: {self.current_submission.title}')
+    self.current_submission.comments.replace_more(limit=max_replace_more_count)  
+    self.current_submission_comments_retrieved = True
     
   def get_reddit_conversation(self, max_length=10):
-    for submission in self.submissions:
-      print(f'Processing submission {submission.title}')
-      submission.comments.replace_more(limit=None)
-      print('Finished replace_more')
-      conversation = []
-      for top_level_comment in submission.comments:
-        print(top_level_comment.body)
-        conversation = [top_level_comment.body]
-        conversation.extend(self.get_conversation_statements(top_level_comment.replies[0], max_length-1))
-        return conversation
+      print(f'Processing submission {self.current_submission.title}')
+      if not self.current_submission_comments_retrieved:
+        self.retrieve_submission_comments()
+      return self.get_conversation_from_current_submission(max_length)  
 
+  def get_conversation_from_current_submission(self, max_length):
+    conversation = []
+    if not self.current_submission.comments:
+      return conversation
+    self.current_comment = (self.current_submission.comments[0])
+    print(self.current_comment.body)
+    conversation = [self.current_comment.body]
+    if not self.current_comment.replies:
+      return conversation
+    conversation.extend(self.get_conversation_statements(self.current_comment.replies[0], max_length-1))
+    return conversation
 
   def get_conversation_statements(self, comment, conversation_length_remaining):
     print(f'Conversation length remaining {conversation_length_remaining}')
